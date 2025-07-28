9fc17c7d2bf14f5f673308abdae10b740fcdcfd9
AieleniAbhinavReddy <23951A0503@iare.ac.in>
2025-07-28 10:23:49 +0530

    Time: 2 ms (15.85%) | Memory: 41.5 MB (93.93%) - LeetSync

---
+++ 91-decode-ways/decode-ways.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numDecodings(String s) {
+        if(s==null || s.length()==0 || s.charAt(0)=='0') return 0;
+        int n=s.length();
+        int[] dp=new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            int singled=Character.getNumericValue(s.charAt(i-1));
+            int doubled=Integer.parseInt(s.substring(i-2,i));
+            if(singled>=1 && singled<=9){
+                dp[i]+=dp[i-1];
+            }
+            if(doubled>=10 && doubled<=26){
+                dp[i]+=dp[i-2];
+            }
+        }
+        return dp[n];
+    }
+}
