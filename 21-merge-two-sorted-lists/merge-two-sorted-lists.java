f4491196f957b7795f855ac4c88640785e808ead
AieleniAbhinavReddy <23951A0503@iare.ac.in>
2025-09-12 13:33:52 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (59.96%) - LeetSync

---
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.java
@@ -0,0 +1,55 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1==null) return list2;
+        if(list2==null) return list1;
+        int temp=0;
+        if(list1.val<list2.val){
+            temp=list1.val;
+            list1=list1.next;
+        }else{
+            temp=list2.val;
+            list2=list2.next;
+        }
+        ListNode head=new ListNode(temp);
+        ListNode curr=head;
+        while(list1 !=null && list2 != null){
+            if(list1.val<list2.val){
+                // ListNode newNode=new ListNode(list1.val);
+                // curr.next=newNode;
+                curr.next=list1;
+                list1=list1.next;
+            }else{
+                // ListNode newNode=new ListNode(list2.val);
+                // curr.next=newNode;
+                curr.next=list2;
+                list2=list2.next;
+            }
+            curr=curr.next;
+        }
+        while(list1!=null){
+            // ListNode newNode=new ListNode(list1.val);
+            // curr.next=newNode;
+            curr.next=list1;
+            curr=curr.next;
+            list1=list1.next;
+        }
+        while(list2!=null){
+            // ListNode newNode=new ListNode(list2.val);
+            // curr.next=newNode;
+            curr.next=list2;
+            curr=curr.next;
+            list2=list2.next;
+        }
+        return head;
+    }
+}
