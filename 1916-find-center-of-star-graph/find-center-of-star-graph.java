5405eafa665e0436e0d4ade46ed7849fb1e05b86
AieleniAbhinavReddy <23951A0503@iare.ac.in>
2025-10-09 15:48:42 +0530

    Time: 29 ms (10.95%) | Memory: 89.8 MB (5.98%) - LeetSync

---
+++ 1916-find-center-of-star-graph/find-center-of-star-graph.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int findCenter(int[][] edges) {
+        List<List<Integer>> adj=new ArrayList<>();
+        Set<Integer> nodes=new HashSet<>();
+        for(int[] e:edges){
+            nodes.add(e[0]);
+            nodes.add(e[1]);
+        }
+        for(int i=1;i<=nodes.size();i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int[] e:edges){
+            int u=e[0],v=e[1];
+            adj.get(u-1).add(v);
+            adj.get(v-1).add(u);
+        }
+        for(int i=1;i<=nodes.size();i++){
+            if(adj.get(i-1).size()==nodes.size()-1) return i;
+        }
+        return -1;
+    }
+}
