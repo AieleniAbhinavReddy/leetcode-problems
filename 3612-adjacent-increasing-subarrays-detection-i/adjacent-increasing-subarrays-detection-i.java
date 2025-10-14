bebf1d0cf1a220108ffb7aa4a7052bc440184cc3
AieleniAbhinavReddy <23951A0503@iare.ac.in>
2025-10-14 10:28:34 +0530

    Time: 3 ms (20.59%) | Memory: 44.3 MB (80.88%) - LeetSync

--- 3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.java
+++ 3612-adjacent-increasing-subarrays-detection-i/adjacent-increasing-subarrays-detection-i.java
@@ -1,20 +1,32 @@
 class Solution {
     private static boolean checkIncreasing(List<Integer> nums,int s,int k){
-        // if(s-s+k==1) return true;
         for(int i=s;i<s+k-1;i++){
             if(i+1==nums.size()) return false;
             if(nums.get(i)>=nums.get(i+1)) return false;
         }
         return true;
     }
+    // public boolean hasIncreasingSubarrays(List<Integer> nums, int k) {
+    //     int i=0,j=k;
+    //     while(i<nums.size() && j<nums.size()){
+    //         if(checkIncreasing(nums,i,k) && checkIncreasing(nums,j,k)){
+    //             return true;
+    //         }
+    //         i++;
+    //         j++;
+    //     }
+    //     return false;
+    // }
     public boolean hasIncreasingSubarrays(List<Integer> nums, int k) {
-        int i=0,j=k;
-        while(i<nums.size() && j<nums.size()){
-            if(checkIncreasing(nums,i,k) && checkIncreasing(nums,j,k)){
-                return true;
+        boolean[] check=new boolean[nums.size()];
+        // Arrays.fill(check,false);
+        for(int i=0;i<nums.size();i++){
+            if(checkIncreasing(nums,i,k)){
+                check[i]=true;
             }
-            i++;
-            j++;
+        }
+        for(int i=0;i+k<check.length;i++){
+            if(check[i] && check[i+k]) return true;
         }
         return false;
     }
