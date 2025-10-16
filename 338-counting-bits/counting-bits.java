382655132ce14f0f2a203a7fbb6442fdccd395f5
AieleniAbhinavReddy <23951A0503@iare.ac.in>
2025-10-16 13:54:43 +0530

    Time: 2 ms (94.29%) | Memory: 46.7 MB (52.86%) - LeetSync

--- 338-counting-bits/counting-bits.java
+++ 338-counting-bits/counting-bits.java
@@ -1,9 +1,9 @@
 class Solution {
     public int[] countBits(int n) {
-        int[] dp=new int[n+1];
-        for(int i=1;i<=n;i++){
-            dp[i]=dp[i/2]+(i%2);
+        int[] res=new int[n+1];
+        for(int i=0;i<res.length;i++){
+            res[i]=res[i>>1]+(i&1);
         }
-        return dp;
+        return res;
     }
 }
