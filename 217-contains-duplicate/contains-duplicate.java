8b66411a38dc0cc4a8ccce9ea1c3268e40609303
AieleniAbhinavReddy <23951A0503@iare.ac.in>
2025-12-06 14:28:04 +0530

    Time: 12 ms (89.05%) | Memory: 97.8 MB (5.06%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -2,8 +2,8 @@ class Solution {
     public boolean containsDuplicate(int[] nums) {
         Set<Integer> set=new HashSet<>();
         for(int n:nums){
-            if(set.contains(n)) return true;
-            set.add(n);
+            if(!set.add(n)) return true;
+            //set.add(n);
         }
         return false;
     }
