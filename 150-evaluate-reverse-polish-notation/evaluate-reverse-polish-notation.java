551b6ebada16f2edf22b3d19a7e059f84db59b05
AieleniAbhinavReddy <23951A0503@iare.ac.in>
2025-12-17 09:58:14 +0530

    Time: 7 ms (41.11%) | Memory: 45.4 MB (33.97%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -15,30 +15,5 @@ class Solution {
             }
         }
         return s.pop();
-        // Stack<Integer> s=new Stack<>();
-        // int i=0;
-        // while(i<tokens.length){
-        //     String curr=tokens[i++];
-        //     if(curr.equals("+")){
-        //         int res=s.pop()+s.pop();
-        //         s.push(res);
-        //     }else if(curr.equals("-")){
-        //         int a=s.pop(),b=s.pop();
-        //         int res=b-a;
-        //         s.push(res);
-        //     }else if(curr.equals("*")){
-        //         int res=s.pop()*s.pop();
-        //         s.push(res);
-        //     }else if(curr.equals("/")){
-        //         int a=s.pop(),b=s.pop();
-        //         int res=b/a;
-        //         s.push(res);
-        //     }else{
-        //         s.push(Integer.parseInt(curr));
-        //     }
-        //     System.out.println(curr);
-        // }
-        // if(s.isEmpty()) return -1;
-        // return s.pop();
     }
 }
