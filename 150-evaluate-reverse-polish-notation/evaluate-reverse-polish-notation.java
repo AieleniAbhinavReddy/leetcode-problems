bb74060ecd017a1270ad3eea331c2a607b556331
AieleniAbhinavReddy <23951A0503@iare.ac.in>
2025-12-17 09:57:59 +0530

    Time: 7 ms (41.11%) | Memory: 45.1 MB (65.60%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -4,26 +4,41 @@ class Solution {
         int i=0;
         while(i<tokens.length){
             String curr=tokens[i++];
-            if(curr.equals("+")){
-                int res=s.pop()+s.pop();
-                s.push(res);
-            }else if(curr.equals("-")){
+            if("+-*/".contains(curr)){
                 int a=s.pop(),b=s.pop();
-                int res=b-a;
-                s.push(res);
-            }else if(curr.equals("*")){
-                int res=s.pop()*s.pop();
-                s.push(res);
-            }else if(curr.equals("/")){
-                int a=s.pop(),b=s.pop();
-                int res=b/a;
-                s.push(res);
+                if(curr.equals("+")) s.push(a+b);
+                if(curr.equals("-")) s.push(b-a);
+                if(curr.equals("*")) s.push(a*b);
+                if(curr.equals("/")) s.push(b/a);
             }else{
                 s.push(Integer.parseInt(curr));
             }
-            System.out.println(curr);
         }
-        if(s.isEmpty()) return -1;
         return s.pop();
+        // Stack<Integer> s=new Stack<>();
+        // int i=0;
+        // while(i<tokens.length){
+        //     String curr=tokens[i++];
+        //     if(curr.equals("+")){
+        //         int res=s.pop()+s.pop();
+        //         s.push(res);
+        //     }else if(curr.equals("-")){
+        //         int a=s.pop(),b=s.pop();
+        //         int res=b-a;
+        //         s.push(res);
+        //     }else if(curr.equals("*")){
+        //         int res=s.pop()*s.pop();
+        //         s.push(res);
+        //     }else if(curr.equals("/")){
+        //         int a=s.pop(),b=s.pop();
+        //         int res=b/a;
+        //         s.push(res);
+        //     }else{
+        //         s.push(Integer.parseInt(curr));
+        //     }
+        //     System.out.println(curr);
+        // }
+        // if(s.isEmpty()) return -1;
+        // return s.pop();
     }
 }
