59a8be8289f303d3a1e0aa15bb0096cbea7e8122
AieleniAbhinavReddy <23951A0503@iare.ac.in>
2025-12-17 09:52:41 +0530

    Time: 56 ms (5.72%) | Memory: 46.5 MB (11.56%) - LeetSync

--- 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
+++ 150-evaluate-reverse-polish-notation/evaluate-reverse-polish-notation.java
@@ -1,38 +1,29 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<Integer> stack=new Stack<>();
+        Stack<Integer> s=new Stack<>();
         int i=0;
-        List<String> opr=new ArrayList<>();
-        opr.add("+");
-        opr.add("-");
-        opr.add("*");
-        opr.add("/");
         while(i<tokens.length){
-            if(opr.contains(tokens[i])){
-                if(stack.isEmpty()) return -1;
-                int a=stack.pop();
-                int b=stack.pop();
-                int res;
-                if(tokens[i].equals("+")){
-                    res=b+a;
-                    stack.push(res);
-                }else if(tokens[i].equals("-")){
-                    res=b-a;
-                    stack.push(res);
-                }else if(tokens[i].equals("*")){
-                    res=b*a;
-                    stack.push(res);
-                }else if(tokens[i].equals("/")){
-                    res=b/a;
-                    stack.push(res);
-                }
+            String curr=tokens[i++];
+            if(curr.equals("+")){
+                int res=s.pop()+s.pop();
+                s.push(res);
+            }else if(curr.equals("-")){
+                int a=s.pop(),b=s.pop();
+                int res=b-a;
+                s.push(res);
+            }else if(curr.equals("*")){
+                int res=s.pop()*s.pop();
+                s.push(res);
+            }else if(curr.equals("/")){
+                int a=s.pop(),b=s.pop();
+                int res=b/a;
+                s.push(res);
             }else{
-                int t=Integer.parseInt(tokens[i]);
-                stack.push(t);
+                s.push(Integer.parseInt(curr));
             }
-            i++;
+            System.out.println(curr);
         }
-        if(stack.isEmpty()) return -1;
-        return stack.pop();
+        if(s.isEmpty()) return -1;
+        return s.pop();
     }
 }
