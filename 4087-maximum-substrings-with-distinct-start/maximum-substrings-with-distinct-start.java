acf7e52012724cf3456443735ecc00656ff51a4b
AieleniAbhinavReddy <23951A0503@iare.ac.in>
2025-12-17 12:13:06 +0530

    Time: 7 ms (69.21%) | Memory: 48.1 MB (11.34%) - LeetSync

--- 4087-maximum-substrings-with-distinct-start/maximum-substrings-with-distinct-start.java
+++ 4087-maximum-substrings-with-distinct-start/maximum-substrings-with-distinct-start.java
@@ -1,9 +1,33 @@
 class Solution {
     public int maxDistinct(String s) {
-        Set<Character> set=new HashSet<>();
+        int[] seen=new int[26];
+        int ct=0;
         for(char c:s.toCharArray()){
-            set.add(c);
+            int i=c-97;
+            if(seen[i] != 1){
+                seen[i]=1;
+                ct++;
+            }
         }
-        return set.size();
+        return ct;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        // Set<Character> set=new HashSet<>();
+        // for(char c:s.toCharArray()){
+        //     set.add(c);
+        // }
+        // return set.size();
