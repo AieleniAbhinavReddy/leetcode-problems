ceb245eab0467cd05aafd5fff7b1cd95ac394d93
AieleniAbhinavReddy <23951A0503@iare.ac.in>
2025-12-17 10:55:31 +0530

    Time: 6 ms (31.93%) | Memory: 43.6 MB (31.54%) - LeetSync

---
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String removeOuterParentheses(String s) {
+        Stack<Character> st=new Stack<>();
+        StringBuilder sb=new StringBuilder();
+        for(char c:s.toCharArray()){
+            if(c=='('){
+                if(!st.isEmpty()) sb.append(c);
+                st.push(c);
+            }else{
+                st.pop();
+                if(!st.isEmpty()) sb.append(c);
+            } 
+        }
+        for(char c:st){
+            sb.append(c);
+        }
+        return sb.toString();
+    }
+}
