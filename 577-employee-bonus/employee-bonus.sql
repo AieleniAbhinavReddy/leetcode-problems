fd19db6299ba58d3bbb986bcc53cf381f64bb60c
AieleniAbhinavReddy <23951A0503@iare.ac.in>
2025-12-18 14:33:17 +0530

    Time: 938 ms (44.05%) | Memory: 0B (100.00%) - LeetSync

--- 577-employee-bonus/employee-bonus.sql
+++ 577-employee-bonus/employee-bonus.sql
@@ -1,5 +1,11 @@
-select name,bonus from Employee e
-left join bonus b
+-- select name,bonus from Employee e
+-- left join bonus b
+-- on e.empId=b.empId
+-- where bonus<1000
+-- or bonus is null
+
+select name,bonus from Bonus b
+right join Employee e
 on e.empId=b.empId
 where bonus<1000
 or bonus is null
